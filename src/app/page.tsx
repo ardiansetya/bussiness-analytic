3aff9bd3638328916d2ceaa748fa0e754a3a11ac
ardiansetya <ardiansetya129@gmail.com>
2025-12-05 01:27:18 +0700

    fix error import global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { headers } from "next/headers";
-import Link from "next/link";
-import { redirect } from "next/navigation";
 
-import { LatestPost } from "@/app/_components/post";
-import { auth } from "@/server/better-auth";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import { getSession } from "@/server/better-auth/server";
 import { api, HydrateClient } from "@/trpc/server";
-import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default async function Home() {
 	const hello = await api.post.hello({ text: "from tRPC" });
